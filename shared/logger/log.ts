22622c43f79f0aa4353c1e6565e40390eb4d411c
Sergei Solovev <sergey.soloviev@inbox.ru>
2022-05-26 15:12:16 +0300

    logger format improvement

--- shared/logger/log.ts
+++ shared/logger/log.ts
@@ -2,5 +2,5 @@ export function log(type: 'HTTP' | 'WebSocket' | 'System', text: string, id: str
     const typeConstWidth = type + ' '.repeat(9).slice(type.length);
     const message = [new Date().toLocaleString(), '|', typeConstWidth, `(${id})`, ':', text];
     console.log(...message)
-    return message.join(', ');
+    return message.join(' ');
 }
